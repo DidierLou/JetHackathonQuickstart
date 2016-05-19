e7fee2fce7ea5b4b5a637235455e647b794f2191
Ed Shnekendorf <eshneken@yahoo.com>
2016-05-19 13:39:49 -0400

    Add dynamic graph code

--- doc_root/js/viewModels/performance.js
+++ doc_root/js/viewModels/performance.js
@@ -1,9 +1,69 @@
 /**
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
+ *
+ Performance content module
+ *
  */
-/* 
- * Your viewModel code goes here
- */
+define(['ojs/ojcore' ,'knockout', 'ojs/ojselectcombobox', 'ojs/ojchart'
+   ], function(oj, ko) {
 
+   /**
+    * The view model for the main content view template
+    */
+    function mainContentViewModel() {
+        var self = this;
+        //var root = 'http://localhost:3000';
+        var root = 'https://javatwittermicroservice-metcsgse00210.apaas.us2.oraclecloud.com/';
 
+        /******************************************************************
+         Callback methods
+         *****************************************************************/
+        this.getTimeSeriesData = function(id) {
+            var url = root + '/timeseries/' + id;
+            $.ajax({
+                url: url,
+                method: 'GET',
+                async: true
+            }).success(function (result) {
+                self.barSeriesValue(result.timeSeriesData);
+                self.barGroupsValue(result.timeSeriesGroups);
+            });
+        }
+
+        this.optionChangedHandler = function (event, data) {
+          if (data.option == "value") {
+            self.getTimeSeriesData(data.value[0]);
+          }
+        }
+
+        /******************************************************************
+         Main Body
+         *****************************************************************/
+
+        // get select list names/values
+        self.availableDataSets = ko.observableArray();
+        var url = root + '/timeseriesIndex';
+            $.ajax({
+                url: url,
+                method: 'GET',
+                async: false
+            }).success(function (result) {
+                //console.log(result.data);
+                self.availableDataSets(result.data);
+            });
+
+        // get initial dataset
+        self.dataSetVal = ko.observableArray([self.availableDataSets()[0].value]);
+        self.getTimeSeriesData(self.dataSetVal()[0]);
+
+        // bind observables for graph type, stack value, and orientation
+        self.graphVal = ko.observableArray(["bar"]);
+        self.stackValue = ko.observable('off');
+        self.orientationValue = ko.observable('vertical');
+
+        // bind dataset observables
+        self.barSeriesValue = ko.observableArray();
+        self.barGroupsValue = ko.observableArray();
+    }
+
+   return mainContentViewModel;
+});
